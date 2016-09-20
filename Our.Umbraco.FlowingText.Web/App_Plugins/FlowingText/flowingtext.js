7ba77c54cee053529fa0be6e3cf11484680d4843
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-09-21 00:23:09 +0200

    Prototype done

---
+++ Our.Umbraco.FlowingText.Web/App_Plugins/FlowingText/flowingtext.js
@@ -0,0 +1,114 @@
+﻿    (function () {
+        var umbraco;
+    
+        function FlowingTextController(scope, editorState) {
+            var model = scope.control,
+                ti, pi, 
+                tab,
+                prop,
+                items = {},
+                properties = [],
+                watch,
+                sourceProperty,
+                sourcePropertySetting = model.editor.config.settings["source-property"];
+
+            function alreadyRenderedFragments() {
+                var sections = scope.model.value.sections,
+                    section, si, row, ri, area, ai, control, ci,
+                    fragments = 0;
+                for (si = 0; si < sections.length; si++) {
+                    section = sections[si];
+                    for (ri = 0; ri < section.rows.length; ri++) {
+                        row = section.rows[ri];
+                        for (ai = 0; ai < row.areas.length; ai++) {
+                            area = row.areas[ai];
+                            for (ci = 0; ci < area.controls.length; ci++) {
+                                control = area.controls[ci];
+                                if (control === model) {
+                                    return fragments;
+                                }
+                                if (control.editor.alias === model.editor.alias &&
+                                    control.config["source-property"] === model.config["source-property"]) {
+                                    fragments += control.config.fragments;
+                                }
+                            }
+                        }
+                    }
+                }
+                return fragments;
+            }
+
+            function updateFlow() {
+                var elements,
+                    begin = alreadyRenderedFragments(),
+                    end = begin + model.config.fragments;
+                if (!sourceProperty) {
+                    return;
+                }
+                elements = $.grep($(sourceProperty.value), function(e) {
+                    return !(e instanceof Text);
+                });
+                model.value =
+                    $("<div>").append(
+                        $(Array.prototype.slice.call(elements, begin, end || elements.length))
+                    )
+                    .html();
+            }
+
+            sourcePropertySetting.config = angular.extend(
+                sourcePropertySetting.config,
+                { items: items }
+            );
+
+            for (ti = 0; ti < editorState.current.tabs.length; ti++) {
+                tab = editorState.current.tabs[ti];
+                for (pi = 0; pi < tab.properties.length; pi++) {
+                    prop = tab.properties[pi];
+                    properties.push(prop);
+                    items[prop.alias] = prop.label;
+                }
+            }
+
+            if (!model.config) {
+                model.config = {};
+                scope.editGridItemSettings(model, 'control');
+            }
+
+            scope.$watch("control.config['fragments']", updateFlow);
+
+            scope.$watch("control.config['source-property']", function() {
+                if (model.config["source-property"]) {
+                    sourceProperty = $.grep(properties, function(p) {
+                        return p.alias === model.config["source-property"];
+                    })[0];
+
+                    if (watch) {
+                        watch();
+                    }
+
+                    watch = scope.$watch(function() {
+                        return sourceProperty.value;
+                    }, updateFlow);
+                }
+            });
+        }
+
+        function initializeUmbraco() {
+            try {
+                umbraco = angular.module("umbraco");
+            } catch (e) {
+            }
+
+            if (!umbraco) {
+                umbraco = angular.module("umbraco", []);
+            }
+        }
+
+        initializeUmbraco();
+        umbraco.controller("our.umbraco.flowingtext.controller", [
+            "$scope",
+            "editorState",
+            FlowingTextController
+        ]);
+
+    }());
---
+++ Our.Umbraco.FlowingText.Web/App_Plugins/FlowingText/tests/flowingtext.tests.js
@@ -0,0 +1,188 @@
+﻿/// <reference path="../../../Umbraco/lib/jquery/jquery.min.js" />
+/// <reference path="/Umbraco/lib/angular/1.1.5/angular.js" />
+/// <reference path="/Umbraco/lib/angular/1.1.5/angular-mocks.js" />
+/// <reference path="../flowingtext.js" />
+(function () {
+
+    describe("Flowing text grid editor", function () {
+
+        var rootScope,
+            scope,
+            controllerFactory,
+            defaultEditorState = {
+                current: {
+                    tabs: [
+                        {
+                            alias: "Content",
+                            properties: [
+                                {
+                                    alias: "bodyText",
+                                    label: "Body text"
+                                }
+                            ]
+                        },
+                        {
+                            alias: "Layout",
+                            properties: [
+                                {
+                                    alias: "content",
+                                    label: "Content"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            },
+            model = {
+                "editor": {
+                    "alias": "flowingText",
+                    "config": {
+                        "settings": {
+                            "source-property": {
+                                "label": "Source property",
+                                "key": "source-property",
+                                "description": "The source for the text",
+                                "view": "/umbraco/views/propertyeditors/dropdown/dropdown.html",
+                                "config": {
+                                    "items": {
+                                    }
+                                }
+                            },
+                            "fragments": {
+                                "label": "Fragments to show",
+                                "key": "fragments",
+                                "description": "How many paragraphs or fragments to show",
+                                "view": "/umbraco/views/propertyeditors/integer/integer.html",
+                                "config": {
+
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            gridModel = {
+                value: {
+                    sections: [
+                        {
+                            rows: [
+                                {
+                                    areas: [
+                                        {
+                                            controls: [
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+            editGridItemSettings = function () { };
+
+        beforeEach(module("umbraco"));
+        beforeEach(
+            inject(function ($controller, $rootScope) {
+                controllerFactory = $controller;
+                rootScope = $rootScope;
+                rootScope.editGridItemSettings = editGridItemSettings;
+                rootScope.model = gridModel;
+                scope = rootScope.$new();
+                scope.control = angular.extend({}, model);
+                controllerFactory("our.umbraco.flowingtext.controller", {
+                    "$scope": scope,
+                    "editorState": angular.extend({}, defaultEditorState)
+                });
+            })
+        );
+
+        it("populates the source property options with the properties of the document", function () {
+
+            var sourcePropertySetting = scope.control.editor.config.settings["source-property"];
+
+            expect(sourcePropertySetting.config.items).toEqual({
+                "bodyText": "Body text",
+                "content": "Content"
+            });
+
+        });
+
+        it("sets the value when a source property is selected", function () {
+            var expectedText = "<p>The expected value</p>",
+                initialText = "<p>Unexpected value</p>",
+                property = defaultEditorState.current.tabs[0].properties[0];
+
+            property.value = initialText;
+
+            scope.control.config = {
+                "source-property": "bodyText"
+            };
+            scope.$digest();
+
+            // we have to do double digest here to simulate update to the text
+            property.value = expectedText;
+            scope.$digest();
+
+            expect(scope.control.value).toBe(expectedText);
+        });
+
+        it("gets a limited amount of fragments", function () {
+            var expectedText =
+                    "<h1>Expected header</h1>" +
+                    "<p>The expected value</p>",
+                allText =
+                    expectedText +
+                    "<p>Unexpected value</p>",
+                property = defaultEditorState.current.tabs[0].properties[0];
+
+            property.value = allText;
+
+            scope.control.config = {
+                "source-property": "bodyText",
+                "fragments": 2
+            };
+            scope.$digest();
+
+            expect(scope.control.value).toBe(expectedText);
+        });
+
+        it("gets the fragments remaining from the previous ones", function() {
+            var expectedText =
+                    "<p>Expected paragraph</p>" +
+                    "<p>Another expected paragraph</p>",
+                allText =
+                    "<h1>Unexpected header</h1>" +
+                    expectedText +
+                    "<p>Unexpected value</p>",
+                property = defaultEditorState.current.tabs[0].properties[0];
+
+            property.value = allText;
+
+            scope.control.config = {
+                "source-property": "bodyText",
+                "fragments": 2
+            };
+
+            gridModel.value.sections[0].rows[0].areas[0].controls = [
+                {
+                    editor: {
+                        alias: "flowingText"
+                    },
+                    config: {
+                        "source-property": "bodyText",
+                        fragments: 1
+                    }
+                },
+                scope.control
+            ];
+
+            scope.$digest();
+
+            expect(scope.control.value).toBe(expectedText);
+        });
+
+    });
+
+}());
+
